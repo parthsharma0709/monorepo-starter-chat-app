b9997a38bda00c8656491fe0ca06bcc76d705a7a
Parth <05sharmaparth@gmail.com>
2025-03-20 14:57:21 +0530

    Added http-server, ws-server and a common backends.json in typescript-config.json

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,26 @@
+import express, { Response , Request} from "express"
+const app= express();
+app.use(express.json());
+
+app.post("/signup", (req :Request, res:Response)=> {
+    res.send("hello world")
+
+})
+
+
+
+app.post("/signin", (req :Request, res:Response)=> {
+    res.send("hello world")
+
+})
+
+app.get("/chat", (req :Request, res:Response)=> {
+    res.send("hello from chat endpoint")
+
+})
+
+
+
+app.listen(3001, ()=>{
+    console.log("http-server is listening on port 3001")
+})
